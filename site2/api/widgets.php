2644594fbe785443c48d79ba4f9035e342ba166a
Bryan <bcchaiklin@gmail.com>
2013-04-20 08:43:42 -0400

    initial commit

---
+++ site2/api/widgets.php
@@ -0,0 +1,10 @@
+<?php
+// header("Access-Control-Allow-Origin: http://site1.com:8888");
+// header("Access-Control-Allow-Methods: PUT");
+// header("Access-Control-Allow-Headers: X-FOO-BAR");
+
+echo "site2 widgets";
+
+// header("Content-Type: application/json");
+// echo $_GET['callback'] . '(' . "[1, 2, 3, 4, 5]" . ')';
+
